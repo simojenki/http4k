f3fbd95eabbace8c5e0052bebd9f81c76d83fca1
Simon J <simojenki@users.noreply.github.com>
2018-03-24 11:20:23 +0800

    cleaning up messages for app demonstrating GET request problem against F5

--- http4k-client-apache/src/test/kotlin/ApacheFundInfoApp.kt
+++ http4k-client-apache/src/test/kotlin/ApacheFundInfoApp.kt
@@ -1,18 +1,29 @@
 import org.http4k.client.ApacheClient
 import org.http4k.core.BodyMode
+import org.http4k.core.HttpHandler
 import org.http4k.core.Method.GET
 import org.http4k.core.Request
 import org.http4k.core.Status
 
-fun main(args: Array<String>) {
-    val http = ApacheClient(requestBodyMode = BodyMode.Stream)
-    val response = http(Request(GET, "https://api.fundinfo.com/"))
+fun execute(name : String, http : HttpHandler) {
+    val url = "https://api.fundinfo.com/"
 
+    val response = http(Request(GET, url))
+
+    println("GET $url using $name returned ${response.status}")
     if(response.status != Status.OK) throw RuntimeException("Got ${response.status}")
 
     if(response.bodyString().matches(".*error.*".toRegex(setOf(RegexOption.DOT_MATCHES_ALL, RegexOption.IGNORE_CASE)))) {
-        throw RuntimeException("Body contains error string")
+        println("!!! Body for $name contains an error")
+    } else {
+        println("Body for $name contains no error")
     }
+}
+
+fun main(args: Array<String>) {
+    execute("BodyMode.Memory", ApacheClient(requestBodyMode = BodyMode.Memory))
+
+    execute("BodyMode.Stream", ApacheClient(requestBodyMode = BodyMode.Stream))
 
     println("done..")
 }
